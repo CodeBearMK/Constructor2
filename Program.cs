3e8cfd915fafa278fb0b31659c26249b07987583
cat32tw <mike1993xd@gmail.com>
2022-02-13 16:59:56 +0800

    建構函式練習2

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Constructor2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect1 = new Rectangle();
+            Rectangle rect2 = new Rectangle(12);
+            Rectangle rect3 = new Rectangle(15,10);
+            Console.WriteLine();
+            Console.Read();
+        }
+    }
+}
+
---
+++ Rectangle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor2
+{
+    internal class Rectangle
+    {
+
+        public int Length { get; set; }
+        public int Wide { get; set; }
+
+        public Rectangle()
+        {
+            Length = 10; Wide = 10; 
+            Console.WriteLine("矩形一 採 Rectangle()預設建構函式");
+            Console.WriteLine("長度:{0}, 寬度:{1}, 面積:{2}", Length, Wide, GetArea());
+        }
+        public Rectangle(int L)
+        {
+            Length = L; Wide = 10;
+            Console.WriteLine("矩形二 採 Rectangle({0})建構函式", L);
+            Console.WriteLine("長度:{0}, 寬度:{1}, 面積:{2}", L, Wide, GetArea());
+
+        }
+        public Rectangle(int L, int W)
+        {
+            Length = L;
+            Wide = W;
+            Console.WriteLine("矩形三 採 Rectangle({0},{1})建構函式", L, W);
+            Console.WriteLine("長度:{0}, 寬度:{1}, 面積:{2}", L, W, GetArea());
+        }
+        public int GetArea()
+        {
+            return Length * Wide;
+        }
+    }
+}
